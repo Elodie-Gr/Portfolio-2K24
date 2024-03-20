060c7fe2ecd04df0141ce7c48be40d7c2de68530
Elodie G <83244962+Elodie-Gr@users.noreply.github.com>
2024-03-20 13:38:30 +0100

    Update Contact

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -1,10 +1,16 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled, { keyframes, css } from 'styled-components';
 import { useInView } from 'react-intersection-observer';
 
 const Contact = () => {
   const titleRef = useRef();
   const [inViewRef, inView] = useInView();
+  const [formData, setFormData] = useState({
+    email: '',
+    name: '',
+    subject: '',
+    message: ''
+  });
 
   useEffect(() => {
     const title = titleRef.current;
@@ -28,11 +34,73 @@ const Contact = () => {
     });
   };
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
   return (
     <ContactContainer>
       <TitleContainer>
         <Title ref={(node) => { inViewRef(node); titleRef.current = node; }}>Contact</Title>
       </TitleContainer>
+      <FormContainer>
+        <Form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <FormField>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </FormField>
+          <FormField>
+            <label htmlFor="name">Nom:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </FormField>
+          <FormField>
+            <label htmlFor="subject">Sujet:</label>
+            <input
+              type="text"
+              id="subject"
+              name="subject"
+              value={formData.subject}
+              onChange={handleChange}
+              required
+            />
+          </FormField>
+          <FormField fullwidth>
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            ></textarea>
+          </FormField>
+          <SubmitButton type="submit">Submit</SubmitButton>
+        </Form>
+      </FormContainer>
     </ContactContainer>
   );
 };
@@ -88,6 +156,78 @@ const TitleContainer = styled.div`
 
 const Title = styled(AnimatedText)`
   letter-spacing: 4px;
+`;
+
+const FormContainer = styled.div`
+  position: absolute;
+  top: 60%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  max-width: 600px;
+  margin-top: 50px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const FormField = styled.div`
+  width: 100%;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+
+  label {
+    color: white;
+    margin-bottom: 5px;
+  }
+
+  input,
+  textarea {
+    width: 100%;
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    background-color: #333;
+    color: white;
+    font-size: 16px;
+    transition: background-color 0.3s;
+  }
+
+  textarea {
+    height: 150px;
+  }
+
+  input[type="text"]:focus,
+  input[type="email"]:focus,
+  textarea:focus {
+    outline: none;
+  }
+
+  input[type="text"]::placeholder,
+  input[type="email"]::placeholder,
+  textarea::placeholder {
+    color: #FFDEE4;
+  }
+`;
+
+
+const SubmitButton = styled.button`
+  padding: 10px 150px;
+  background-color: #FFDEE4;
+  border: none;
+  border-radius: 5px;
+  color: black;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #FFDEE4;
+  }
 `;
 
 export default Contact;
